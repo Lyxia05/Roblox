5e845475e0313b96d45406a7f0081c67c1227dc9
Lyxia <daozagetrich6@gmail.com>
2024-11-16 09:57:52 +0700

    a

--- Tools/dex.lua
+++ Tools/dex.lua
@@ -4,4 +4,4 @@ loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Dex%20Explorer.txt"))
 
 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Lyxia05/Roblox/refs/heads/main/DungeonQuest/DungeonQuest.lua?token=GHSAT0AAAAAAC2RUUA6JKZJ4ZLOX7IC7JBYZZYBFXA"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lyxia05/Roblox/refs/heads/main/DungeonQuest/DungeonQuest.lua?token=GHSAT0AAAAAAC2RUUA7QYKUWYUY6BY2WZIWZZYBHIA"))()
