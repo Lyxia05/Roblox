3ee27ba9ba0430a9caa852e1676f91966c05f10a
Lyxia <daozagetrich6@gmail.com>
2024-11-21 05:23:09 +0700

    asd

--- DungeonQuest/DungeonQuest.lua
+++ DungeonQuest/DungeonQuest.lua
@@ -139,13 +139,13 @@ local function AutoFarming()
 
     local _distance = (CURRENT_OBJECT:GetPivot().Position - Character.HumanoidRootPart.Position).Magnitude
 
-    if _distance <= CURRENT_OBJECT.HumanoidRootPart.Size.Y + 20 then
+    if _distance <= CURRENT_OBJECT.HumanoidRootPart.Size.Y + 8 then
         if SAVED_TWEEN ~= nil then
             SAVED_TWEEN:Cancel()
         end
         ClipEnabled = true
         game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
-        Character.HumanoidRootPart.CFrame = CURRENT_OBJECT:GetPivot() * CFrame.new(0, CURRENT_OBJECT.HumanoidRootPart.Size.Y + 10, 0) * CFrame.Angles(math.rad(-90), 0, math.rad(90))
+        Character.HumanoidRootPart.CFrame = CURRENT_OBJECT:GetPivot() * CFrame.new(0, CURRENT_OBJECT.HumanoidRootPart.Size.Y + 8, 0) * CFrame.Angles(math.rad(-90), 0, math.rad(90))
     else
         if ClipEnabled == true then
             return
--- Tools/dex.lua
+++ Tools/dex.lua
@@ -1,7 +1,7 @@
 loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Dex%20Explorer.txt"))()
 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Lyxia05/Roblox/refs/heads/main/DungeonQuest/DungeonQuest.lua?token=GHSAT0AAAAAAC2RUUA737HYKWVVC2A5GVFYZZ6L74A"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lyxia05/Roblox/refs/heads/main/DungeonQuest/DungeonQuest.lua?token=GHSAT0AAAAAAC2RUUA7C5GKWSXZSDIKCJNMZZ6MDRQ"))()
 
 
 workspace.kerse.HumanoidRootPart.BodyPosition.Position = workspace.Value.Value.Position + Vector3.new(0, 30 ,0)
