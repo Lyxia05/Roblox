5ef9cf5695d2c01e26cb64e74030c17200cf28a8
Lyxia <daozagetrich6@gmail.com>
2024-11-21 03:45:37 +0700

    asd

--- Tools/dex.lua
+++ Tools/dex.lua
@@ -1,7 +1,7 @@
 loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Dex%20Explorer.txt"))()
 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Lyxia05/Roblox/refs/heads/main/DungeonQuest/DungeonQuest.lua?token=GHSAT0AAAAAAC2RUUA62KWG5BCTARHONIXIZZ6JINQ"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lyxia05/Roblox/refs/heads/main/DungeonQuest/DungeonQuest.lua?token=GHSAT0AAAAAAC2RUUA6DZHDILRBXNDQHTRWZZ6JJIQ"))()
 
 
 workspace.kerse.HumanoidRootPart.BodyPosition.Position = workspace.Value.Value.Position + Vector3.new(0, 30 ,0)
