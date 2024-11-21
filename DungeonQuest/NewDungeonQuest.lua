53b3054791fa34ef25201caa7bbf8d781e06f80b
Lyxia <daozagetrich6@gmail.com>
2024-11-21 15:36:07 +0700

    asd

--- DungeonQuest/NewDungeonQuest.lua
+++ DungeonQuest/NewDungeonQuest.lua
@@ -15,7 +15,7 @@ local LocalPlayer = Players.LocalPlayer
 local DungeonFolder = workspace.dungeon
 
 --
-local Speed = 50
+local Speed = 100
 local CURRENT_OBJECT = nil
 
 ----- AUTO FARM SECTION -----
