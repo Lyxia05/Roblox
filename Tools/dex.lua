66266c43c555314495af2fba68619732cf72d955
Lyxia <daozagetrich6@gmail.com>
2024-11-24 06:02:41 +0700

    asd

--- Tools/dex.lua
+++ Tools/dex.lua
@@ -1,5 +1,8 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
 
+getgenv().Enabled = true
+getgenv().WeaponType = "LargeWeapon" -- Sword / Staff / Dual Sword
+getgenv().WaitInterval = 0.01
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lyxia05/Roblox/refs/heads/main/DungeonQuest/NewDungeonQuest.lua"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lyxia05/Roblox/refs/heads/main/DungeonQuest/NewDungeonQuest.lua?token=GHSAT0AAAAAAC2RUUA7CAWWJX62ME7XYBY4Z2BMGLA"))()
@@ -18,3 +21,9 @@ end
 
 
 workspace.dungeonProgress.Value = "bossKilled"
+
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+Character.Head.playerNameplate.HealthBar.Health.Text = "480.50Q/480.50Q"
--- kinayo.lua
+++ kinayo.lua
@@ -1,4 +1,3 @@
------ ANIMATION DATA -----
 local ANIMATION_TABLE = {
     ["LargeWeapon"] = {
         ["Idle"] = 87776815396170,
@@ -33,59 +32,58 @@ local ANIMATION_TABLE = {
         ["Attacks"] = { "rbxassetid://13740049031", "rbxassetid://13740048897" },
         ["TempIgnoreAttackKeyFrame"] = true
     }
-}
-
-print(tick())
-
------ SETTINGS -----
-getgenv().Enabled = true
-getgenv().WeaponType = "LargeWeapon" -- Sword / Staff / Dual Sword
-getgenv().WaitInterval = 0.01
-
---
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-
---
-local AnimationObject = nil
-local Animation = nil
-
-local function GetCharacter()
+  }
+  
+  getgenv().Enabled = true
+  getgenv().WeaponType = "LargeWeapon"
+  getgenv().WaitInterval = 0
+  
+  local Players = game:GetService("Players")
+  local LocalPlayer = Players.LocalPlayer
+  local AnimationObject = nil
+  local Animation = nil
+  
+  local function GetCharacter()
     return LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
-end
-
-local function UpdateAnimationByIndex(index)
+  end
+  
+  local function UpdateAnimationByIndex(index)
     AnimationObject.AnimationId = ANIMATION_TABLE[getgenv().WeaponType].Attacks[index]
-end
-
-local function GetKillAuraArgs(index)
-    return {[1] = {{ ["animationIndex"] = index, ["sentAt"] = tick() + (tick() % 1) }, "\151"}}
-end
-
-local function CreateAnimation()
+  end
+  
+  local function GetKillAuraArgs(index)
+    return {[1] = {{ ["animationIndex"] = index, ["sentAt"] = tick() }, "\151"}}
+  end
+  
+  local function CreateAnimation()
     AnimationObject = Instance.new("Animation")
     AnimationObject.Parent = GetCharacter()
     UpdateAnimationByIndex(1)
-
     return AnimationObject
-end
-
-local function CharacterAdded(character)
+  end
+  
+  local function CharacterAdded(character)
     CreateAnimation()
     local Humanoid = character:WaitForChild("Humanoid")
     Animation = Humanoid:LoadAnimation(CreateAnimation())
-    character.Head.playerNameplate.PlayerName.Text = "Hewwo :3"
-end
-
---
-LocalPlayer.CharacterAdded:Connect(CharacterAdded)
-task.spawn(function()
+  end
+  
+  LocalPlayer.CharacterAdded:Connect(CharacterAdded)
+  
+  task.spawn(function()
     local CurrentIndex = 0
     CharacterAdded(GetCharacter())
     while getgenv().Enabled do
         if Animation ~= nil and GetCharacter() ~= nil and GetCharacter():FindFirstChild("HumanoidRootPart") then
             CurrentIndex += 1
-            game:GetService("ReplicatedStorage").dataRemoteEvent:FireServer(unpack(GetKillAuraArgs(CurrentIndex)))
+            for i = 1, 5 do
+                for j = 1, 3 do
+                    game:GetService("ReplicatedStorage").dataRemoteEvent:FireServer(unpack(GetKillAuraArgs(CurrentIndex)))
+                end
+                task.spawn(function()
+                    game:GetService("ReplicatedStorage").dataRemoteEvent:FireServer(unpack(GetKillAuraArgs(CurrentIndex)))
+                end)
+            end
             UpdateAnimationByIndex(CurrentIndex)
             Animation:Play()
             Animation:Stop()
@@ -94,6 +92,16 @@ task.spawn(function()
                 CurrentIndex = 0
             end
         end
-        task.wait()
+        task.wait(0)
     end
-end)
+  end)
+  
+  spawn(function()
+    while wait() do
+        pcall(function()
+            for i = 1, 3 do
+                game:GetService("ReplicatedStorage").dataRemoteEvent:FireServer(unpack(GetKillAuraArgs(i)))
+            end
+        end)
+    end
+  end)
